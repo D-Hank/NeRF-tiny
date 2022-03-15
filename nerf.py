74e9cd2f5c9a68ff68fbd55cec0e6dc446956c1b
D-Hank <78422438+D-Hank@users.noreply.github.com>
2022-03-15 17:14:51 +0800

    Add files via upload

--- nerf.py
+++ nerf.py
@@ -92,47 +92,26 @@ class NeRFModel():
     # Get cdf of coarse sampling, then with its reverse, we use uniform sampling along the horizontal axis
     def resample(self, t_coarse, sigma_coarse):
         cdf = torch.cumsum(sigma_coarse, dim = 0)
-        '''
-        plt.figure()
-        plt.plot(t_coarse.numpy(), cdf.numpy(), label='PDF of values')
-        plt.legend()
-        plt.xlabel('Value')
-        plt.ylabel('Probability')
-        output_image = "ex.png"
-        plt.savefig(output_image)
-        plt.show()
-        '''
         high = max(cdf)
         low = min(cdf)
         delta = t_coarse[1] - t_coarse[0]
-        delta_inv = (high - low) / self.num_fine
         # Slope of cdf is not zero, so its inverse is not infinite
         slope = sigma_coarse[1: ] / delta
         slope_inv = 1.0 / slope
         t_inv = torch.linspace(low, high, self.num_fine + 2)
-        # Ignore first and last
-        #print(t_inv)
-        #t_inv = t_inv[1:-1]
         # Init value
-        #lower_cdf = torch.clone(cdf[1:-1])
-        #lower_t = torch.clone(t_coarse[1:-1])
-        t_fine = torch.rand(self.num_fine - 1, 1)
-        for i in range(0, self.num_fine + 2):
+        #print(t_inv)
+        t_inv = t_inv[1:-1]
+        t_fine = torch.rand(self.num_fine, 1)
+        for i in range(0, self.num_fine):
             # select those less than
             sel_le = torch.nonzero(t_inv[i] > cdf)
             sel_le = sel_le[-1]
-            #lower_cdf[i] = cdf[sel_le]
-            #lower_t[i] = t_coarse[sel_le]
+            # Linear increment
             t_fine[i] = t_coarse[sel_le] + (t_inv[i] - cdf[sel_le]) * slope_inv[sel_le]
-            #print(sel_le)
 
-        #print(lower_t)
-        #print(lower_cdf)
-        print(t_fine)
-        exit(0)
-        #t_fine = torch.mul((t_inv - lower_cdf), slope_inv) + lower_t
-        print(t_coarse)
-        print(t_fine)
+        #print(t_coarse)
+        #print(t_fine)
 
 
     # Render a ray
@@ -160,7 +139,11 @@ class NeRFModel():
 
         color_fi, sigma_fi = self.resample(t_coarse, sigma_co)
 
+'''
+#t_c = torch.tensor([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110])
+#s_c = torch.tensor([0.1, 0.15, 0.23, 0.3, 0.5, 0.8, 1.2, 0.9, 0.6, 0.38, 0.2])
 t_c = torch.tensor([100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110])
-s_c = torch.tensor([0.1, 0.15, 0.23, 0.3, 0.5, 0.8, 1.2, 0.9, 0.6, 0.38, 0.2])
+s_c = torch.tensor([1.2, 0.9, 0.8, 0.5, 0.3, 0.6,  0.38, 0.1, 0.15, 0.23, 0.2])
 nerf = NeRFModel(num_fine = 10)
 nerf.resample(t_c, s_c)
+'''
