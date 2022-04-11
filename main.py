b076635d41934d27a6369fa2fdcade01c71479e7
D-Hank <78422438+D-Hank@users.noreply.github.com>
2022-04-11 23:36:48 +0800

    Fix the problem of continuing

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from nerf import NeRFRunner
 
 CONF_DIR = "./conf/"
 
+# -----------------------------------START OF EVERYTHING----------------------------------
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "NeRF argument parser.")
     parser.add_argument("--conf", type = str, default = "fern")
@@ -13,23 +15,23 @@ if __name__ == "__main__":
     conf = ConfigParser()
     conf.read(CONF_DIR + args.conf + ".ini")
 
-    gpu = int(conf.get(args.conf, "GPU"))
-    img_dir = conf.get(args.conf, "IMG_DIR")
-    results_path = conf.get(args.conf, "RESULTS_PATH")
-    ckpt_path = conf.get(args.conf, "CKPT_PATH")
-    low_res = int(conf.get(args.conf, "LOW_RES"))
-    total_iter = int(conf.get(args.conf, "TOTAL_ITER"))
-    batch_ray = int(conf.get(args.conf, "BATCH_RAY"))
-    learning = float(conf.get(args.conf, "LEARNING"))
-    lr_gamma = float(conf.get(args.conf, "LR_GAMMA"))
-    lr_milestone = list(conf.get(args.conf, "LR_MILESTONE"))
-    n_coarse = int(conf.get(args.conf, "N_COARSE"))
-    n_fine = int(conf.get(args.conf, "N_FINE"))
-    data_type = conf.get(args.conf, "DATA_TYPE")
-    step = int(conf.get(args.conf, "STEP"))
-    decay_end = float(conf.get(args.conf, "DECAY_END"))
-    sched = conf.get(args.conf, "SCHED")
-    continu = bool(conf.get(args.conf, "CONTINU"))
+    gpu             =   int(conf.get(args.conf, "GPU"))
+    img_dir         =       conf.get(args.conf, "IMG_DIR")
+    results_path    =       conf.get(args.conf, "RESULTS_PATH")
+    ckpt_path       =       conf.get(args.conf, "CKPT_PATH")
+    low_res         =   int(conf.get(args.conf, "LOW_RES"))
+    total_iter      =   int(conf.get(args.conf, "TOTAL_ITER"))
+    batch_ray       =   int(conf.get(args.conf, "BATCH_RAY"))
+    learning        = float(conf.get(args.conf, "LEARNING"))
+    lr_gamma        = float(conf.get(args.conf, "LR_GAMMA"))
+    lr_milestone    =  list(conf.get(args.conf, "LR_MILESTONE"))
+    n_coarse        =   int(conf.get(args.conf, "N_COARSE"))
+    n_fine          =   int(conf.get(args.conf, "N_FINE"))
+    data_type       =       conf.get(args.conf, "DATA_TYPE")
+    step            =   int(conf.get(args.conf, "STEP"))
+    decay_end       = float(conf.get(args.conf, "DECAY_END"))
+    sched           =       conf.get(args.conf, "SCHED")
+    continu         =  bool(conf.get(args.conf, "CONTINU"))
 
     run_nerf = NeRFRunner(
         gpu = gpu,
